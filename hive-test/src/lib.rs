a8cd6a66f6a32fdd5a23418951a56f1450d14ba7
Thierry Kühni <thierry.kuehni@hotmail.com>
2022-04-12 22:06:55 +0200

    changed probe handle type in Hive testfunctions from Probe to Session

--- hive-test/src/lib.rs
+++ hive-test/src/lib.rs
@@ -1,14 +1,14 @@
 pub use controller::runner::TestChannelHandle;
 pub use hive_macro::hive_test;
 pub use inventory;
-use probe_rs_test::Probe;
+use probe_rs_test::Session;
 
 #[allow(dead_code)]
 pub struct HiveTestFunction {
     pub name: &'static str,
     pub ordered: usize,
     pub should_panic: bool,
-    pub test_fn: fn(test_channel: &mut dyn TestChannelHandle, probe: &mut Probe),
+    pub test_fn: fn(test_channel: &mut dyn TestChannelHandle, session: &mut Session),
 }
 
 inventory::collect!(HiveTestFunction);
