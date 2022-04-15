5125ef6cbbd3a1e2cdfe1adcba706bf885eb7222
Thierry Kühni <thierry.kuehni@hotmail.com>
2022-04-15 19:06:43 +0200

    added test result types

--- comm-types/src/results.rs
+++ comm-types/src/results.rs
@@ -2,6 +2,21 @@
 
 use serde::{Deserialize, Serialize};
 
-/// Test result format
+/// A single test result
 #[derive(Debug, Serialize, Deserialize)]
-pub struct TestResults {}
+pub struct TestResult {
+    pub status: TestStatus,
+    pub should_panic: bool,
+    pub test_name: String,
+    pub target_name: String,
+    pub probe_name: String,
+    pub probe_sn: String,
+}
+
+/// Status of a test, failed and skipped contain the reason for the skipping/failure
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TestStatus {
+    PASSED,
+    FAILED(String),
+    SKIPPED(String),
+}
