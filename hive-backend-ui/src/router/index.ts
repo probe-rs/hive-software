6b03e96249c533e87b122c8e60535850a9abf5fc
Thierry Kühni <thierry.kuehni@hotmail.com>
2022-05-10 20:02:30 +0200

    added not found view

--- hive-backend-ui/src/router/index.ts
+++ hive-backend-ui/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Overview from "@/views/Overview.vue";
 import Testprograms from "@/views/Testprograms.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
       name: "testprograms",
       component: Testprograms,
     },
+    { path: '/:pathMatch(.*)*', component: NotFound },
   ],
 });
 
